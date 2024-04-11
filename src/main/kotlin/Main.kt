d1a12ca43971a5c5473cc03b79515c0e743468ec
jacobrein <jacob.rein@gabb.com>
2024-04-11 08:28:28 -0600

    Update compose dependencies and Kotlin version
    Upgraded compose dependencies by adding compose.material3 and compose.materialIconsExtended and updated the Kotlin version. Main.kt file was updated which includes creating custom composables such as EmptyCard and a switch to modify the real-time shader. The code was also refactored to use the newly added dependencies.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,110 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CopyAll
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.intellij.lang.annotations.Language
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 @Preview
 fun App() {
     var shaderText by remember { mutableStateOf(defaultShaderText) }
     var shaderText2 by remember { mutableStateOf(defaultShaderText) }
-    var realtime by remember { mutableStateOf(false) }
+    var realtime by remember { mutableStateOf(true) }
+
+    val shader by remember(shaderText) {
+        derivedStateOf {
+            object : Shader {
+                override val name: String
+                    get() = ""
+                override val authorName: String
+                    get() = ""
+                override val authorUrl: String
+                    get() = ""
+                override val credit: String
+                    get() = ""
+                override val license: String
+                    get() = ""
+                override val licenseUrl: String
+                    get() = ""
+                override val sksl: String
+                    get() = shaderText
+
+            }
+        }
+    }
 
     LaunchedEffect(shaderText2, realtime) {
-        if(realtime) {
+        if (realtime) {
             shaderText = shaderText2
         }
     }
 
-    MaterialTheme(darkColors()) {
+    MaterialTheme(darkColorScheme()) {
         Surface {
             Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = { Text("Shader Maker") },
+                        actions = {
+                            val clipboard = LocalClipboardManager.current
+                            IconButton(
+                                onClick = {
+                                    clipboard.setText(
+                                        buildAnnotatedString { append(shaderText2) }
+                                    )
+                                }
+                            ) { Icon(Icons.Default.CopyAll, null) }
+                        }
+                    )
+                },
                 bottomBar = {
                     BottomAppBar {
-                        Text("Realtime Shader")
-                        Switch(
-                            realtime,
-                            onCheckedChange = { realtime = it },
-                        )
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Text("Realtime Shader")
+                                Switch(
+                                    realtime,
+                                    onCheckedChange = { realtime = it },
+                                )
+                            }
 
-                        Button(
-                            onClick = { shaderText = shaderText2 }
-                        ) { Text("Submit") }
+                            Button(
+                                onClick = { shaderText = shaderText2 }
+                            ) { Text("Submit") }
+
+                            Button(
+                                onClick = {
+                                    shaderText2 = shaderText2
+                                        .replace("iTime", "uTime")
+                                        .replace("iResolution", "uResolution")
+                                }
+                            ) { Text("Replace i with u") }
+                        }
                     }
                 }
             ) { padding ->
@@ -48,40 +114,98 @@ fun App() {
                         .fillMaxSize()
                         .padding(padding)
                 ) {
+                    /*Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                    ) {
+                        EmptyCard(
+                            modifier = Modifier.weight(1f),
+                            cardBack = Modifier.shaderBackground(shader)
+                        )
+
+                        OutlinedCard(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            currentIssue?.stackTraceToString()?.let { Text(it) }
+                        }
+                    }*/
+
                     EmptyCard(
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxHeight(),
-                        cardBack = Modifier.shaderBackground(
-                            remember(shaderText) {
-                                object : Shader {
-                                    override val name: String
-                                        get() = ""
-                                    override val authorName: String
-                                        get() = ""
-                                    override val authorUrl: String
-                                        get() = ""
-                                    override val credit: String
-                                        get() = ""
-                                    override val license: String
-                                        get() = ""
-                                    override val licenseUrl: String
-                                        get() = ""
-                                    override val sksl: String
-                                        get() = shaderText
+                        cardBack = Modifier.shaderBackground(shader)
+                    )
 
+                    var lineTops by remember { mutableStateOf(emptyArray<Float>()) }
+                    val density = LocalDensity.current
+
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                    ) {
+                        BasicTextField(
+                            shaderText2,
+                            onValueChange = { shaderText2 = it },
+                            textStyle = MaterialTheme.typography.bodyLarge.copy(
+                                color = MaterialTheme.colorScheme.onSurface
+                            ),
+                            cursorBrush = Brush.sweepGradient(
+                                listOf(
+                                    MaterialTheme.colorScheme.onSurface,
+                                    MaterialTheme.colorScheme.onSurface
+                                )
+                            ),
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(2.dp)
+                                .fillMaxWidth(),
+                            onTextLayout = { result ->
+                                lineTops = Array(result.lineCount) { result.getLineTop(it) }
+                            },
+                            decorationBox = {
+                                Row(
+                                    modifier = Modifier.verticalScroll(rememberScrollState())
+                                ) {
+                                    if (lineTops.isNotEmpty()) {
+                                        Box(modifier = Modifier.padding(horizontal = 4.dp)) {
+                                            lineTops.forEachIndexed { index, top ->
+                                                Text(
+                                                    modifier = Modifier.offset(y = with(density) { top.toDp() }),
+                                                    text = (index + 1).toString()
+                                                )
+                                            }
+                                        }
+                                    }
+                                    it()
                                 }
                             }
                         )
-                    )
 
-                    TextField(
+                        OutlinedCard(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            currentIssue?.stackTraceToString()?.let {
+                                Text(it, modifier = Modifier.padding(4.dp))
+                            }
+                        }
+                    }
+
+                    /*TextField(
                         shaderText2,
                         onValueChange = { shaderText2 = it },
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxHeight()
-                    )
+                    )*/
                 }
             }
         }
@@ -92,7 +216,7 @@ fun App() {
 fun EmptyCard(
     modifier: Modifier = Modifier,
     shape: Shape = MaterialTheme.shapes.medium,
-    color: Color = MaterialTheme.colors.surface,
+    color: Color = MaterialTheme.colorScheme.surface,
     contentColor: Color = contentColorFor(color),
     border: BorderStroke? = null,
     cardBack: Modifier? = null,
@@ -124,19 +248,19 @@ vec4 main( vec2 fragCoord ) {
     // Output to screen
     return vec4(col,1.0);
 }
-    """
+"""
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
 
-    Window(
+    /*Window(
         onCloseRequest = {},
         transparent = false,
         alwaysOnTop = true
     ) {
-        MaterialTheme(darkColors()) {
+        MaterialTheme(darkColorScheme()) {
             Surface(
                 modifier = Modifier
                     .fillMaxSize()
@@ -145,5 +269,5 @@ fun main() = application {
                 currentIssue?.stackTraceToString()?.let { Text(it) }
             }
         }
-    }
+    }*/
 }
